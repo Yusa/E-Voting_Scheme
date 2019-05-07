97531be8029cedd395df346d1ccae88bf7f7a80c
yusa <yusa.erguven@invictuseurope.com>
2019-05-08 02:08:34 +0300

    tally.py initialized

---
+++ tally.py
@@ -0,0 +1,20 @@
+
+def ShamirSecretSharing(n, t, s, q):
+	return
+
+
+def PartialDecryption(X, a, Lambda, t, p):
+	return
+
+
+def FullDecryption(Y, a, Omega, G, t, p, q):
+	return
+
+
+def CheckQuorum(a, h_a_lambda, p, q):
+	return
+
+
+def ZK_commonexp(Lambda_i, h_a_lambda_i, Omega_i, p, q):
+	return
+
